f8fc0010cea4aa6bcb72fadfd1659970cb67f766
RUSSELL STATHER <russellstather@RUSSELLs-MacBook-Air.local>
2015-08-19 20:56:40 +0100

    Deploy

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: russellstather
+ * Date: 18/08/15
+ * Time: 21:17
+ */
+require __DIR__ . '/vendor/autoload.php';
+
+
+
+$app = new \Slim\Slim();
+$app->get('/books/:id', function ($id) use ($app){
+
+//$app->render('output.php');
+echo 'hi there';
+});
+
+$app->get('foo', function() use ($app){
+
+});
+
+$app->run();
+
---
+++ output.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: russellstather
+ * Date: 18/08/15
+ * Time: 22:54
+ */
+echo ("hello world");
