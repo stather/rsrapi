9574459ed0b92d14b837dbc1c25461d24878c88f
RUSSELL STATHER <russellstather@RUSSELLs-MacBook-Air.local>
2015-09-01 22:18:50 +0100

    Deploy

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -15,35 +15,27 @@ class ComposerAutoloaderInitcec96723d0319b2e97b4cd6f803b8648
 
     public static function getLoader()
     {
-        echo "m0";
         if (null !== self::$loader) {
             return self::$loader;
         }
-echo "m1";
         spl_autoload_register(array('ComposerAutoloaderInitcec96723d0319b2e97b4cd6f803b8648', 'loadClassLoader'), true, true);
-        echo "m2";
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
-        echo "m3";
         spl_autoload_unregister(array('ComposerAutoloaderInitcec96723d0319b2e97b4cd6f803b8648', 'loadClassLoader'));
-        echo "m4";
 
         $map = require __DIR__ . '/autoload_namespaces.php';
         foreach ($map as $namespace => $path) {
             $loader->set($namespace, $path);
         }
-        echo "m5";
 
         $map = require __DIR__ . '/autoload_psr4.php';
         foreach ($map as $namespace => $path) {
             $loader->setPsr4($namespace, $path);
         }
-        echo "m6";
 
         $classMap = require __DIR__ . '/autoload_classmap.php';
         if ($classMap) {
             $loader->addClassMap($classMap);
         }
-        echo "m7";
 
         $loader->register(true);
 
@@ -51,7 +43,6 @@ echo "m1";
         foreach ($includeFiles as $file) {
             composerRequirecec96723d0319b2e97b4cd6f803b8648($file);
         }
-        echo "m8";
 
         return $loader;
     }
