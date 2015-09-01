1f720a875c8c2fd85616f8c9c143889eb7bf331f
RUSSELL STATHER <russellstather@RUSSELLs-MacBook-Air.local>
2015-09-01 21:49:11 +0100

    Deploy

--- controllers/FoodController.php
+++ controllers/FoodController.php
@@ -13,6 +13,8 @@ use com\readysteadyrainbow\models\Food;
 use com\readysteadyrainbow\models\ListFoodsModel;
 use com\readysteadyrainbow\utility\ImageProcessor;
 
+echo "foodcontroller";
+
 class FoodController extends Controller
 {
     private $colour;
